03e9a6c61c428422a1e7b588ee01dde6bbd96a07
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2023-04-28 03:54:44 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,3 +1,4 @@
+import java.util.*;
 // Create a Student class with private instance variables for the student's name, ID number, and averageMark.
 // The class should include public getter and setter methods for each variable.
 
@@ -6,64 +7,49 @@
 
 // The main method should create three Student objects and use the setter methods to set the name, ID number, and averageMark for each student.
 //Then, it should use the getter methods to display the information for each student.
-import java.util.*;
-public class Student {
+
+public class Main {
+    //private instances created
     private String name;
     private int idNum;
     private double averageMark;
     public static void main(String[] args) {
-        Scanner inputObj = new Scanner(System.in);
-        System.out.println("Enter name of the first student ");     //get the first name
-        String name1 = inputObj.nextLine();
-        System.out.println("Enter id num of the first student ");     // get the first id
-        String id1 = inputObj.nextInt();
-        System.out.println("Enter average mark of the first student ");     //get the first avgMark
-        String avg1 = inputObj.nextDouble();
-        Student student1 = new Student(name1, id1, avg1);       //first student object
+        String[] students = {};  //Create array
 
-        System.out.println("Enter name of the second student ");     //get the second name
-        String name2 = inputObj.nextLine();
-        System.out.println("Enter id num of the second student ");     // get the second id
-        String id2 = inputObj.nextInt();
-        System.out.println("Enter average mark of the second student ");     
-        String avg2 = inputObj.nextDouble();             //get the second avg
-        Student student2 = new Student(name2, id2, avg2);       //second student object
-
-        System.out.println("Enter name of the third student ");     //get the third name
-        String name3 = inputObj.nextLine(); 
-        System.out.println("Enter id num of the third student ");     // get the third id
-        String id3 = inputObj.nextInt();
-        System.out.println("Enter average mark of the third student ");     //get the third avgMark
-        String avg3 = inputObj.nextDouble();
-        Student student3 = new Student(name3, id3, avg3);       //third student object
-
-
-
-
-        // for (i=1;i>3;i++) {
-        //     Dictionary<Integer, String> dict= new Hashtable<>();
-        //     dict.put(1,"first");
-        //     dict.put(2, "second");
-        //     dict.put(3, "third");
-    
-            // Student studObj = new Student();
+        for (int i=0;i<3;i++) {
+            Scanner inputObj = new Scanner(System.in);
+            System.out.println("Enter name of the student ");     //get the first name
+            String name1 = inputObj.nextLine();
+            System.out.println("Enter id num of the student ");     // get the first id
+            int id1 = inputObj.nextInt();
+            System.out.println("Enter average mark of the student ");     //get the first avgMark
+            double avg1 = inputObj.nextDouble();
+            Main student = new Main(name1,id1,avg1);        //create object
+            String student1 = (student.getName()+"(ID #"+ student.getIdNum()+"): "+ student.getAverageMark());      //ad everything to a string
+            ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(students));
+            arrayList.add(student1);        //add string to list
+            students = arrayList.toArray(students);   
         }
-    
-    Student(String nameA, int idNumA, double averageMarkA) {
-        this.nameA = name;
-        this.idNumA = idNum;
-        this.averageMarkA = averageMark;
+        for (int i = 0; i<3;i++){
+            System.out.println(students[i]);            //print each item of the list
+        }
     }
-    public double getAverageMark() {
-        return averageMark;
-    }
-    public int getIdNum() {
-        return idNum;
-    }
+
+  Main(String name, int idNum, double averageMark) {            //constructor
+    this.name = name;
+    this.idNum = idNum;
+    this.averageMark = averageMark;
+  }
+  public double getAverageMark() {          //getters
+      return averageMark;
+  }
+  public int getIdNum() {
+      return idNum;
+  }
     public String getName() {
         return name;
     }
-    public void setAverageMark(double averageMark) {
+    public void setAverageMark(double averageMark) {            //setters
         this.averageMark = averageMark;
     }
     public void setIdNum(int idNum) {
@@ -72,5 +58,4 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
-
 }
